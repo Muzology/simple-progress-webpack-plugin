fe51457cbe6f8ef1496c69f71a71861aa6dc248d
Jason Duncan <jason@muzology.com>
2019-11-14 04:13:20 -0600

    Update compact-logger.js

--- logger/compact-logger.js
+++ logger/compact-logger.js
@@ -25,6 +25,7 @@ module.exports = function CompactLogger( options ) {
 	// Initial log
 	let logLines = [];
 	// log( 'Webpack: Starting ...' );
+	log('compact logger created');
 
 	/**
 	 * Use the webpack-internal progress plugin as the base of the logger
